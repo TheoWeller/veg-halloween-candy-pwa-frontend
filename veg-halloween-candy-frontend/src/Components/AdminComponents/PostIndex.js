5ce30377b6fd4e213160efae14218962a614869e
Theo Weller <weller.theodor@gmail.com>
2019-08-16 18:52:24 -0400

    sorting function added to render function in PostIndex

--- veg-halloween-candy-frontend/src/Components/AdminComponents/PostIndex.js
+++ veg-halloween-candy-frontend/src/Components/AdminComponents/PostIndex.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Component, Fragment} from 'react';
+import { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
@@ -8,6 +8,7 @@ import CreatePostCard from './CreatePostCard'
 class PostIndex extends Component {
   //renderPosts works off of draftView to render posts
   renderPosts = (posts) => {
+    const sortedPosts = posts.sort((a,b) => (a.rank > b.rank) ? 1 : ((b.rank > a.rank) ? -1 : 0));
     return posts.map(post => {
       if(post.draft === this.props.draftView){
         return(
