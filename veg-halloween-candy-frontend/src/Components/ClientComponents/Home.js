99cd2f5a400cab2f2e335b14f6c8464cb3ba4a6b
Theo Weller <weller.theodor@gmail.com>
2019-08-23 13:34:22 -0400

    refactored hook in Home.js to prevent race conditions

--- veg-halloween-candy-frontend/src/Components/ClientComponents/Home.js
+++ veg-halloween-candy-frontend/src/Components/ClientComponents/Home.js
@@ -16,10 +16,12 @@ export default function CreatePostCard(props){
     return days;
   }
 
-  useEffect(async () => {
-    const response = await fetch("http://localhost:3000/api/v1/posts")
-    const data = await response.json()
-    setPosts(data)
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:3000/api/v1/posts");
+      const data = await response.json();
+      setPosts(data);
+    })();
   }, [])
 
   const renderPosts = (posts) => {
