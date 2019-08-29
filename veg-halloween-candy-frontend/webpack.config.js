dc115539a1352903f63d80fff2cb4024eb09fe04
Theo Weller <weller.theodor@gmail.com>
2019-08-29 14:55:13 -0400

    webpack compiled successfully but not properly displaying

--- veg-halloween-candy-frontend/webpack.config.js
+++ veg-halloween-candy-frontend/webpack.config.js
@@ -1,21 +1,50 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.export = {
+module.exports = {
+  mode: "production",
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, '/dist'),
+    path: path.resolve(__dirname, "dist"),
     filename: 'bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+          presets: ['@babel/preset-env', "@babel/react"]
+          }
         }
-      }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+      test: /\.ttf$/,
+      use: [
+        {
+          loader: 'ttf-loader',
+          options: {
+            name: './font/[hash].[ext]',
+          },
+        },
+      ]
+    },
+    {
+    test: /\.(jpe?g|png|gif|svg)$/i,
+    loader: 'file-loader'
+}
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: 'index.html'
+    })
+  ]
 }
